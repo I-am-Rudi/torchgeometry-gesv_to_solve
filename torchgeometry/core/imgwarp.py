6e9adbf21218d861af129bb16a17da51b73ed85e
rudi <kevin.rudolf.siebert@iof.fraunhofer.de>
2024-11-21 14:22:20 +0100

    gesv->solve

--- torchgeometry/core/imgwarp.py
+++ torchgeometry/core/imgwarp.py
@@ -255,7 +255,7 @@ def get_perspective_transform(src, dst):
     ], dim=1)
 
     # solve the system Ax = b
-    X, LU = torch.gesv(b, A)
+    X, LU = torch.solve(b, A)
 
     # create variable to return
     batch_size = src.shape[0]
